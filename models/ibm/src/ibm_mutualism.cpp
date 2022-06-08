dbbc67b48a0ec1955cbd5269a9eaa7992a72ac87
bramkuijper <bramkuijper@gmail.com>
2022-06-08 09:21:28 +0100

    various changes

--- models/ibm/src/ibm_mutualism.cpp
+++ models/ibm/src/ibm_mutualism.cpp
@@ -1,34 +1,33 @@
 #include <stdexcept>
 #include "ibm_mutualism.h"
+#include "ibm_parameters.h"
 
-IBM_Mutualism::IBM_Mutualism(const IBM_parameters &params) :
-    par(params)
+// constructor expects a parameter object
+IBM_Mutualism::IBM_Mutualism(IBM_parameters const &params) : // constructors first initialize data members
+    rd{} // initialize random device, see *.h file
+    ,seed{rd()} // initialize seed
+    ,rng_r{seed} // initialize the random number generator
+    ,uniform{0.0,1.0} // initialize the normal distribution
+    ,output_file{params.base_name.c_str()} // initialize the data file by giving it a name
+    ,par{params} // initialize the parameter data member with the constructor argument
 {
-  if (!initialize_simulation())
-  {
-      throw std::runtime_error("Cannot successfully initialize the simulation");
-  }
+    for (
 } // end IBM_Mutualism
-
-// set up the simulation
-bool IBM_Mutualism::initialize_simulation()
-{
-
-}
 
 // the brunt of the code: chooser of events
 void IBM_Mutualism::event_chooser()
 {
     // generate death rate distribution
+    std::double <vector> rate_distribution;
 
     // events:
-    // 1. death of species 1
-
+    // 1a death of species 1
+    // 1b death of species 2
+    // 2. environmental change
     for (int npp1_idx = 0; npp1_idx < par.npp[0]; ++npp1_idx)
     {
         for (int npp2_idx = 0; npp2_idx < par.npp[1]; ++npp2_idx)
         {
-            
         }
     }
 } // end IBM_Mutualism::event_chooser
