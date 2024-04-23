a30755ba18fa9e95e2ca0a983b48fbd7d8505a55
Margaret Bolton <m.a.bolton@hotmail.co.uk>
2024-04-23 10:47:48 +0100

    Taylor and Irwin test batch

--- models/ibm/src/batch_file.sh
+++ models/ibm/src/batch_file.sh
@@ -1,22 +1,22 @@
 # this is a nifty batch file to run multiple simulations sequentially
 # mu_f, mu_s, baseline_survival, f_cost_f_help, s_cost_s_help
 
-./MutualismIBM file_test_TIcopy_below_1 0.01 0.0 0.3 0.00
-./MutualismIBM file_test_TIcopy_below_2 0.01 0.0 0.3 0.00
-./MutualismIBM file_test_TIcopy_below_3 0.01 0.0 0.3 0.00
-./MutualismIBM file_test_TIcopy_below_4 0.01 0.0 0.3 0.00
-./MutualismIBM file_test_TIcopy_below_5 0.01 0.0 0.3 0.00
+./MutualismIBM file_test_TIcopy_below_1 0.01 0.0 0.0 0.3 0.00
+./MutualismIBM file_test_TIcopy_below_2 0.01 0.0 0.0 0.3 0.00
+./MutualismIBM file_test_TIcopy_below_3 0.01 0.0 0.0 0.3 0.00
+./MutualismIBM file_test_TIcopy_below_4 0.01 0.0 0.0 0.3 0.00
+./MutualismIBM file_test_TIcopy_below_5 0.01 0.0 0.0 0.3 0.00
 
-./MutualismIBM file_test_TIcopy_at_1 0.01 0.0 0.2 0.00
-./MutualismIBM file_test_TIcopy_at_2 0.01 0.0 0.2 0.00
-./MutualismIBM file_test_TIcopy_at_3 0.01 0.0 0.2 0.00
-./MutualismIBM file_test_TIcopy_at_4 0.01 0.0 0.2 0.00
-./MutualismIBM file_test_TIcopy_at_5 0.01 0.0 0.2 0.00
+./MutualismIBM file_test_TIcopy_at_1    0.01 0.0 0.0 0.2 0.00
+./MutualismIBM file_test_TIcopy_at_2    0.01 0.0 0.0 0.2 0.00
+./MutualismIBM file_test_TIcopy_at_3    0.01 0.0 0.0 0.2 0.00
+./MutualismIBM file_test_TIcopy_at_4    0.01 0.0 0.0 0.2 0.00
+./MutualismIBM file_test_TIcopy_at_5    0.01 0.0 0.0 0.2 0.00
 
-./MutualismIBM file_test_TIcopy_above_1 0.01 0.0 0.1 0.00
-./MutualismIBM file_test_TIcopy_above_2 0.01 0.0 0.1 0.00
-./MutualismIBM file_test_TIcopy_above_3 0.01 0.0 0.1 0.00
-./MutualismIBM file_test_TIcopy_above_4 0.01 0.0 0.1 0.00
-./MutualismIBM file_test_TIcopy_above_5 0.01 0.0 0.1 0.00
+./MutualismIBM file_test_TIcopy_above_1 0.01 0.0 0.0 0.1 0.00
+./MutualismIBM file_test_TIcopy_above_2 0.01 0.0 0.0 0.1 0.00
+./MutualismIBM file_test_TIcopy_above_3 0.01 0.0 0.0 0.1 0.00
+./MutualismIBM file_test_TIcopy_above_4 0.01 0.0 0.0 0.1 0.00
+./MutualismIBM file_test_TIcopy_above_5 0.01 0.0 0.0 0.1 0.00
 
 
