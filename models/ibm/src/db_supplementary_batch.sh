a2205b2dff804cd5ccca4fe155f31f57d717293f
Margaret Bolton <m.a.bolton@hotmail.co.uk>
2024-09-05 16:42:51 +0100

    db fec help supplementary batch

---
+++ models/ibm/src/db_supplementary_batch.sh
@@ -0,0 +1,346 @@
+# supplementary batch for fecundity help in death birth model
+# baseline survival 0.0:1.0, b/c 1:14/1
+# dispersal = 0.15, npp = 5, bf = 20, DB
+
+./build/MutualismIBM  file_set_TIcopy_u_00_100_f_5  0.0  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_02_100_f_5  0.2  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_04_100_f_5  0.4  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_06_100_f_5  0.6  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_08_100_f_5  0.8  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_10_100_f_5  1.0  1.00   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_050_f_5  0.0  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_02_050_f_5  0.2  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_04_050_f_5  0.4  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_06_050_f_5  0.6  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_08_050_f_5  0.8  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_10_050_f_5  1.0  0.50   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_033_f_5  0.0  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_02_033_f_5  0.2  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_04_033_f_5  0.4  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_06_033_f_5  0.6  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_08_033_f_5  0.8  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_10_033_f_5  1.0  0.33   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_025_f_5  0.0  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_02_025_f_5  0.2  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_04_025_f_5  0.4  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_06_025_f_5  0.6  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_08_025_f_5  0.8  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_10_025_f_5  1.0  0.25   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_020_f_5  0.0  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_02_020_f_5  0.2  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_04_020_f_5  0.4  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_06_020_f_5  0.6  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_08_020_f_5  0.8  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_10_020_f_5  1.0  0.20   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_016_f_5  0.0  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_02_016_f_5  0.2  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_04_016_f_5  0.4  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_06_016_f_5  0.6  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_08_016_f_5  0.8  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_10_016_f_5  1.0  0.16   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_010_f_5  0.0  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_02_010_f_5  0.2  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_04_010_f_5  0.4  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_06_010_f_5  0.6  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_08_010_f_5  0.8  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_10_010_f_5  1.0  0.10   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_007_f_5  0.0  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_02_007_f_5  0.2  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_04_007_f_5  0.4  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_06_007_f_5  0.6  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_08_007_f_5  0.8  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_10_007_f_5  1.0  0.07   
+ 
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_100_f_6  0.0  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_02_100_f_6  0.2  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_04_100_f_6  0.4  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_06_100_f_6  0.6  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_08_100_f_6  0.8  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_10_100_f_6  1.0  1.00   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_050_f_6  0.0  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_02_050_f_6  0.2  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_04_050_f_6  0.4  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_06_050_f_6  0.6  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_08_050_f_6  0.8  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_10_050_f_6  1.0  0.50   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_033_f_6  0.0  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_02_033_f_6  0.2  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_04_033_f_6  0.4  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_06_033_f_6  0.6  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_08_033_f_6  0.8  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_10_033_f_6  1.0  0.33   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_025_f_6  0.0  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_02_025_f_6  0.2  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_04_025_f_6  0.4  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_06_025_f_6  0.6  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_08_025_f_6  0.8  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_10_025_f_6  1.0  0.25   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_020_f_6  0.0  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_02_020_f_6  0.2  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_04_020_f_6  0.4  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_06_020_f_6  0.6  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_08_020_f_6  0.8  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_10_020_f_6  1.0  0.20   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_016_f_6  0.0  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_02_016_f_6  0.2  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_04_016_f_6  0.4  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_06_016_f_6  0.6  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_08_016_f_6  0.8  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_10_016_f_6  1.0  0.16   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_010_f_6  0.0  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_02_010_f_6  0.2  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_04_010_f_6  0.4  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_06_010_f_6  0.6  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_08_010_f_6  0.8  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_10_010_f_6  1.0  0.10   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_007_f_6  0.0  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_02_007_f_6  0.2  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_04_007_f_6  0.4  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_06_007_f_6  0.6  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_08_007_f_6  0.8  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_10_007_f_6  1.0  0.07   
+ 
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_100_f_7  0.0  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_02_100_f_7  0.2  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_04_100_f_7  0.4  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_06_100_f_7  0.6  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_08_100_f_7  0.8  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_10_100_f_7  1.0  1.00   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_050_f_7  0.0  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_02_050_f_7  0.2  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_04_050_f_7  0.4  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_06_050_f_7  0.6  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_08_050_f_7  0.8  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_10_050_f_7  1.0  0.50   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_033_f_7  0.0  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_02_033_f_7  0.2  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_04_033_f_7  0.4  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_06_033_f_7  0.6  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_08_033_f_7  0.8  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_10_033_f_7  1.0  0.33   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_025_f_7  0.0  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_02_025_f_7  0.2  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_04_025_f_7  0.4  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_06_025_f_7  0.6  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_08_025_f_7  0.8  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_10_025_f_7  1.0  0.25   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_020_f_7  0.0  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_02_020_f_7  0.2  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_04_020_f_7  0.4  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_06_020_f_7  0.6  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_08_020_f_7  0.8  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_10_020_f_7  1.0  0.20   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_016_f_7  0.0  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_02_016_f_7  0.2  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_04_016_f_7  0.4  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_06_016_f_7  0.6  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_08_016_f_7  0.8  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_10_016_f_7  1.0  0.16   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_010_f_7  0.0  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_02_010_f_7  0.2  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_04_010_f_7  0.4  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_06_010_f_7  0.6  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_08_010_f_7  0.8  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_10_010_f_7  1.0  0.10   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_007_f_7  0.0  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_02_007_f_7  0.2  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_04_007_f_7  0.4  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_06_007_f_7  0.6  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_08_007_f_7  0.8  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_10_007_f_7  1.0  0.07   
+ 
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_100_f_8  0.0  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_02_100_f_8  0.2  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_04_100_f_8  0.4  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_06_100_f_8  0.6  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_08_100_f_8  0.8  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_10_100_f_8  1.0  1.00   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_050_f_8  0.0  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_02_050_f_8  0.2  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_04_050_f_8  0.4  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_06_050_f_8  0.6  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_08_050_f_8  0.8  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_10_050_f_8  1.0  0.50   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_033_f_8  0.0  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_02_033_f_8  0.2  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_04_033_f_8  0.4  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_06_033_f_8  0.6  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_08_033_f_8  0.8  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_10_033_f_8  1.0  0.33   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_025_f_8  0.0  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_02_025_f_8  0.2  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_04_025_f_8  0.4  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_06_025_f_8  0.6  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_08_025_f_8  0.8  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_10_025_f_8  1.0  0.25   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_020_f_8  0.0  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_02_020_f_8  0.2  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_04_020_f_8  0.4  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_06_020_f_8  0.6  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_08_020_f_8  0.8  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_10_020_f_8  1.0  0.20   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_016_f_8  0.0  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_02_016_f_8  0.2  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_04_016_f_8  0.4  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_06_016_f_8  0.6  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_08_016_f_8  0.8  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_10_016_f_8  1.0  0.16   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_010_f_8  0.0  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_02_010_f_8  0.2  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_04_010_f_8  0.4  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_06_010_f_8  0.6  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_08_010_f_8  0.8  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_10_010_f_8  1.0  0.10   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_007_f_8  0.0  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_02_007_f_8  0.2  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_04_007_f_8  0.4  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_06_007_f_8  0.6  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_08_007_f_8  0.8  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_10_007_f_8  1.0  0.07   
+ 
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_100_f_9  0.0  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_02_100_f_9  0.2  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_04_100_f_9  0.4  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_06_100_f_9  0.6  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_08_100_f_9  0.8  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_10_100_f_9  1.0  1.00   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_050_f_9  0.0  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_02_050_f_9  0.2  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_04_050_f_9  0.4  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_06_050_f_9  0.6  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_08_050_f_9  0.8  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_10_050_f_9  1.0  0.50   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_033_f_9  0.0  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_02_033_f_9  0.2  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_04_033_f_9  0.4  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_06_033_f_9  0.6  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_08_033_f_9  0.8  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_10_033_f_9  1.0  0.33   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_025_f_9  0.0  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_02_025_f_9  0.2  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_04_025_f_9  0.4  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_06_025_f_9  0.6  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_08_025_f_9  0.8  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_10_025_f_9  1.0  0.25   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_020_f_9  0.0  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_02_020_f_9  0.2  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_04_020_f_9  0.4  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_06_020_f_9  0.6  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_08_020_f_9  0.8  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_10_020_f_9  1.0  0.20   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_016_f_9  0.0  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_02_016_f_9  0.2  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_04_016_f_9  0.4  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_06_016_f_9  0.6  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_08_016_f_9  0.8  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_10_016_f_9  1.0  0.16   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_010_f_9  0.0  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_02_010_f_9  0.2  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_04_010_f_9  0.4  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_06_010_f_9  0.6  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_08_010_f_9  0.8  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_10_010_f_9  1.0  0.10   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_007_f_9  0.0  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_02_007_f_9  0.2  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_04_007_f_9  0.4  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_06_007_f_9  0.6  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_08_007_f_9  0.8  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_10_007_f_9  1.0  0.07   
+ 
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_100_f_10  0.0  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_02_100_f_10  0.2  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_04_100_f_10  0.4  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_06_100_f_10  0.6  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_08_100_f_10  0.8  1.00  &
+./build/MutualismIBM  file_set_TIcopy_u_10_100_f_10  1.0  1.00   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_050_f_10  0.0  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_02_050_f_10  0.2  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_04_050_f_10  0.4  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_06_050_f_10  0.6  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_08_050_f_10  0.8  0.50  &
+./build/MutualismIBM  file_set_TIcopy_u_10_050_f_10  1.0  0.50   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_033_f_10  0.0  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_02_033_f_10  0.2  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_04_033_f_10  0.4  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_06_033_f_10  0.6  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_08_033_f_10  0.8  0.33  &
+./build/MutualismIBM  file_set_TIcopy_u_10_033_f_10  1.0  0.33   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_025_f_10  0.0  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_02_025_f_10  0.2  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_04_025_f_10  0.4  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_06_025_f_10  0.6  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_08_025_f_10  0.8  0.25  &
+./build/MutualismIBM  file_set_TIcopy_u_10_025_f_10  1.0  0.25   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_020_f_10  0.0  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_02_020_f_10  0.2  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_04_020_f_10  0.4  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_06_020_f_10  0.6  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_08_020_f_10  0.8  0.20  &
+./build/MutualismIBM  file_set_TIcopy_u_10_020_f_10  1.0  0.20   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_016_f_10  0.0  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_02_016_f_10  0.2  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_04_016_f_10  0.4  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_06_016_f_10  0.6  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_08_016_f_10  0.8  0.16  &
+./build/MutualismIBM  file_set_TIcopy_u_10_016_f_10  1.0  0.16   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_010_f_10  0.0  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_02_010_f_10  0.2  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_04_010_f_10  0.4  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_06_010_f_10  0.6  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_08_010_f_10  0.8  0.10  &
+./build/MutualismIBM  file_set_TIcopy_u_10_010_f_10  1.0  0.10   
+ 
+./build/MutualismIBM  file_set_TIcopy_u_00_007_f_10  0.0  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_02_007_f_10  0.2  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_04_007_f_10  0.4  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_06_007_f_10  0.6  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_08_007_f_10  0.8  0.07  &
+./build/MutualismIBM  file_set_TIcopy_u_10_007_f_10  1.0  0.07   
+ 
+ 
