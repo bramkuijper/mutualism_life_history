4db6a62b5bc422aa469ea0dce4d9ecb6b22919f5
Margaret Bolton <m.a.bolton@hotmail.co.uk>
2025-04-10 17:01:54 +0100

    PF, local dispersal, fec

--- models/ibm/src/parameters.h
+++ models/ibm/src/parameters.h
@@ -47,7 +47,7 @@ struct Parameters {
     // 2 for partner fidelity;
     // 3 for partner fidelity and negotiation
     // TODO: 3 partner fidelity and negotiation
-    int partner_mechanism = 0;
+    int partner_mechanism = 2;
 
     // probability that partners will stay together at the end of each time step
     double fidelity_prob{1.0};
