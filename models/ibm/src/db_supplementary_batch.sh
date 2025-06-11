94cfd706f5d0354efca47b8c6e7d867ecf18c84b
Margaret Bolton <m.a.bolton@hotmail.co.uk>
2025-06-11 10:00:24 +0100

    popsize batch

--- models/ibm/src/db_supplementary_batch.sh
+++ models/ibm/src/db_supplementary_batch.sh
@@ -1,80 +1,160 @@
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_1   0.1   0.167   100
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_1   0.1   0.125   100
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_1   0.1   0.1   100
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_1   0.1   0.083   100
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_1   0.1   0.167   500
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_1   0.1   0.125   500
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_1   0.1   0.1   500
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_1   0.1   0.083   500
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_2   0.1   0.167   100
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_2   0.1   0.125   100
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_2   0.1   0.1   100
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_2   0.1   0.083   100
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_2   0.1   0.167   500
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_2   0.1   0.125   500
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_2   0.1   0.1   500
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_2   0.1   0.083   500
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_3   0.1   0.167   100
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_3   0.1   0.125   100
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_3   0.1   0.1   100
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_3   0.1   0.083   100
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_3   0.1   0.167   500
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_3   0.1   0.125   500
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_3   0.1   0.1   500
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_3   0.1   0.083   500
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_4   0.1   0.167   100
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_4   0.1   0.125   100
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_4   0.1   0.1   100
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_4   0.1   0.083   100
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_4   0.1   0.167   500
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_4   0.1   0.125   500
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_4   0.1   0.1   500
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_4   0.1   0.083   500
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_5   0.1   0.167   100
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_5   0.1   0.125   100
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_5   0.1   0.1   100
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_5   0.1   0.083   100
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_5   0.1   0.167   500
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_5   0.1   0.125   500
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_5   0.1   0.1   500
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_5   0.1   0.083   500
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_6   0.1   0.167   100
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_6   0.1   0.125   100
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_6   0.1   0.1   100
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_6   0.1   0.083   100
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_6   0.1   0.167   500
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_6   0.1   0.125   500
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_6   0.1   0.1   500
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_6   0.1   0.083   500
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_7   0.1   0.167   100
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_7   0.1   0.125   100
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_7   0.1   0.1   100
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_7   0.1   0.083   100
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_7   0.1   0.167   500
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_7   0.1   0.125   500
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_7   0.1   0.1   500
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_7   0.1   0.083   500
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_8   0.1   0.167   100
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_8   0.1   0.125   100
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_8   0.1   0.1   100
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_8   0.1   0.083   100
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_8   0.1   0.167   500
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_8   0.1   0.125   500
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_8   0.1   0.1   500
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_8   0.1   0.083   500
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_9   0.1   0.167   100
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_9   0.1   0.125   100
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_9   0.1   0.1   100
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_9   0.1   0.083   100
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_9   0.1   0.167   500
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_9   0.1   0.125   500
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_9   0.1   0.1   500
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_9   0.1   0.083   500
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_10   0.1   0.167   100
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_10   0.1   0.125   100
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_10   0.1   0.1   100
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_10   0.1   0.083   100
-./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_10   0.1   0.167   500
-./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_10   0.1   0.125   500
-./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_10   0.1   0.1   500
-./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_10   0.1   0.083   500
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_1   0.1   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N100_s_1   0.2   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_1   0.1   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N100_s_1   0.2   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_1   0.1   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N100_s_1   0.2   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_1   0.1   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N100_s_1   0.2   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_1   0.1   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N500_s_1   0.2   0.167   500    
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_1   0.1   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N500_s_1   0.2   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_1   0.1   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N500_s_1   0.2   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_1   0.1   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N500_s_1   0.2   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_2   0.1   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N100_s_2   0.2   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_2   0.1   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N100_s_2   0.2   0.125   100    
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_2   0.1   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N100_s_2   0.2   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_2   0.1   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N100_s_2   0.2   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_2   0.1   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N500_s_2   0.2   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_2   0.1   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N500_s_2   0.2   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_2   0.1   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N500_s_2   0.2   0.1   500    
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_2   0.1   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N500_s_2   0.2   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_3   0.1   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N100_s_3   0.2   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_3   0.1   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N100_s_3   0.2   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_3   0.1   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N100_s_3   0.2   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_3   0.1   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N100_s_3   0.2   0.083   100    
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_3   0.1   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N500_s_3   0.2   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_3   0.1   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N500_s_3   0.2   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_3   0.1   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N500_s_3   0.2   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_3   0.1   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N500_s_3   0.2   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_4   0.1   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N100_s_4   0.2   0.167   100    
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_4   0.1   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N100_s_4   0.2   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_4   0.1   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N100_s_4   0.2   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_4   0.1   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N100_s_4   0.2   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_4   0.1   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N500_s_4   0.2   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_4   0.1   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N500_s_4   0.2   0.125   500    
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_4   0.1   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N500_s_4   0.2   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_4   0.1   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N500_s_4   0.2   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_5   0.1   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N100_s_5   0.2   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_5   0.1   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N100_s_5   0.2   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_5   0.1   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N100_s_5   0.2   0.1   100    
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_5   0.1   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N100_s_5   0.2   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_5   0.1   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N500_s_5   0.2   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_5   0.1   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N500_s_5   0.2   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_5   0.1   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N500_s_5   0.2   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_5   0.1   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N500_s_5   0.2   0.083   500    
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_6   0.1   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N100_s_6   0.2   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_6   0.1   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N100_s_6   0.2   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_6   0.1   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N100_s_6   0.2   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_6   0.1   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N100_s_6   0.2   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_6   0.1   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N500_s_6   0.2   0.167   500    
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_6   0.1   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N500_s_6   0.2   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_6   0.1   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N500_s_6   0.2   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_6   0.1   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N500_s_6   0.2   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_7   0.1   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N100_s_7   0.2   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_7   0.1   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N100_s_7   0.2   0.125   100    
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_7   0.1   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N100_s_7   0.2   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_7   0.1   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N100_s_7   0.2   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_7   0.1   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N500_s_7   0.2   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_7   0.1   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N500_s_7   0.2   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_7   0.1   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N500_s_7   0.2   0.1   500    
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_7   0.1   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N500_s_7   0.2   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_8   0.1   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N100_s_8   0.2   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_8   0.1   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N100_s_8   0.2   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_8   0.1   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N100_s_8   0.2   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_8   0.1   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N100_s_8   0.2   0.083   100    
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_8   0.1   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N500_s_8   0.2   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_8   0.1   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N500_s_8   0.2   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_8   0.1   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N500_s_8   0.2   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_8   0.1   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N500_s_8   0.2   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_9   0.1   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N100_s_9   0.2   0.167   100    
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_9   0.1   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N100_s_9   0.2   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_9   0.1   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N100_s_9   0.2   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_9   0.1   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N100_s_9   0.2   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_9   0.1   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N500_s_9   0.2   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_9   0.1   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N500_s_9   0.2   0.125   500    
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_9   0.1   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N500_s_9   0.2   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_9   0.1   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N500_s_9   0.2   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N100_s_10   0.1   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N100_s_10   0.2   0.167   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N100_s_10   0.1   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N100_s_10   0.2   0.125   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N100_s_10   0.1   0.1   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N100_s_10   0.2   0.1   100    
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N100_s_10   0.1   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N100_s_10   0.2   0.083   100   &
+./MutualismIBM   file_set_DB_popsize_bs01_c06_N500_s_10   0.1   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c06_N500_s_10   0.2   0.167   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c08_N500_s_10   0.1   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c08_N500_s_10   0.2   0.125   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c10_N500_s_10   0.1   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c10_N500_s_10   0.2   0.1   500   &
+./MutualismIBM   file_set_DB_popsize_bs01_c12_N500_s_10   0.1   0.083   500   &
+./MutualismIBM   file_set_DB_popsize_bs02_c12_N500_s_10   0.2   0.083   500    
